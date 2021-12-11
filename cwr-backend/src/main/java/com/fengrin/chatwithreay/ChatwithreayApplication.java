4dbb74ff0895ebd7102e61eea663a5be9f9e3944
Feng Zhang <feng.zhang@compass.com>
2021-12-12 03:01:18 +0800

    init

---
+++ cwr-backend/src/main/java/com/fengrin/chatwithreay/ChatwithreayApplication.java
@@ -0,0 +1,13 @@
+package com.fengrin.chatwithreay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwithreayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatwithreayApplication.class, args);
+	}
+
+}
---
+++ cwr-backend/src/test/java/com/fengrin/chatwithreay/ChatwithreayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengrin.chatwithreay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwithreayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
